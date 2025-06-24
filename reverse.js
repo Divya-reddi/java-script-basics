59dd1dbfe19e2ed9d5ab8047cb879c517aa22af5
Divya <divyasaireddi@gmail.com>
2025-06-24 23:16:07 +0530

    Divya|Reversing a string

---
+++ reverse.js
@@ -0,0 +1,34 @@
+let stringToBeReversed = prompt("Please Enter a String to be reversed!!");
+let reversedString = "";
+
+function reverse(str){
+    console.log(str);
+    for(let i=str.length-1; i>=0; i--){
+         reversedString += str[i];
+    }
+    return reversedString;
+}
+
+console.log(reverse(stringToBeReversed));
+
+//2
+//let str = prompt("Enter String:");
+// let revstr = str.split('').reverse().join('');
+// console.log(revstr);
+
+
+//3
+// function reverseString(str) {
+//     let chars = str.split('');
+//     let left = 0;
+//     let right = chars.length - 1;
+
+//     while (left < right) {
+//         [chars[left], chars[right]] = [chars[right], chars[left]];
+//         left++;
+//         right--;
+//     }
+
+//     return chars.join('');
+// }
+// console.log(reverseString(str));
